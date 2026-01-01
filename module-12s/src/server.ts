a1e4153f83a32faa50a6b2a62c7c8f49da469aee
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-01 22:24:58 +0600

    .env and gitignore done

--- module-12s/src/server.ts
+++ module-12s/src/server.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({path : path.join(process.cwd(), ".env")})
 
 const app = express();
 const port = 5000;
@@ -11,16 +15,53 @@ app.use(express.json());
 //DATABASE
 
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_gAaOUMbIjx12@ep-square-cherry-ahd3uy3d-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+  connectionString: `${process.env.CONNECTION_STRING}`,
 });
 
 
+const initDB = async() =>{
+  try{
+       await pool.query(`
+          CREATE TABLE IF NOT EXISTS users(
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(100) NOT NULL,
+          email VARCHAR(150) UNIQUE NOT NULL,
+          age INT,
+          phone VARCHAR(15),
+          address TEXT,
+          created_at TIMESTAMP DEFAULT NOW(),
+          updated_at TIMESTAMP DEFAULT NOW()
+          )
+        `);
+        console.log("users table created");
 
+    await pool.query(`
+           CREATE TABLE IF NOT EXISTS todos(
+           id SERIAL PRIMARY KEY,
+           user_id INT REFERENCES users(id) ON DELETE CASCADE,
+           title VARCHAR(200) NOT NULL,
+           description TEXT,
+           completed BOOLEAN DEFAULT false,
+           due_date DATE,
+           created_at TIMESTAMP DEFAULT NOW(),
+           updated_at TIMESTAMP DEFAULT NOW()
+           )
+        `)
+        console.log("todos table created");
+  }
+  catch(error){
+    console.log("error message : ");
+  }
+    
+}
+
+initDB();
 
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World! hell yeah my goodness");
 });
+
 
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
@@ -29,6 +70,7 @@ app.post("/", (req: Request, res: Response) => {
     message: "API is working man",
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
