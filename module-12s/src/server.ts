a6061ce9ea12a2c80168ac09e1ac2823a8262d26
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-02 00:00:43 +0600

    done

--- module-12s/src/server.ts
+++ module-12s/src/server.ts
@@ -63,8 +63,33 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
+app.post("/users", async(req: Request, res: Response) => {
+  //console.log(req.body);
+  const {name, email} = req.body;
+
+  try{
+     const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
+
+    // console.log(result.rows[0]);
+    // res.send({ message : "data inserted"})
+
+    res.status(201).json({
+      success : true,
+      message : "data inserted successfully",
+      data : result.rows[0]
+
+    
+    })
+
+  }catch(err : any){
+    res.status(500).json({
+      success : false,
+      message : err.message,
+    
+    })
+  }
+
+
   res.status(201).json({
     success: true,
     message: "API is working man",
