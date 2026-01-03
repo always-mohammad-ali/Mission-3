52198ca0c87ec1920a5d98727f9b2e1e8cc74594
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-04 01:52:40 +0600

    auth login successfully done with creating token using jwtToken for further login in future

--- module-12s/src/modules/auth/auth.route.ts
+++ module-12s/src/modules/auth/auth.route.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { authController } from "./auth.controller";
 
 const router = Router();
+
+// auth/login/
+router.post("/login", authController.loginUser)
 
 export const authRoute = router;
