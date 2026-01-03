23abeacd061bf2dccb285744df0acebf546233de
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-03 17:43:41 +0600

    config added

--- module-12s/src/server.ts
+++ module-12s/src/server.ts
@@ -1,12 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
-import dotenv from "dotenv";
-import path from "path";
+import config from "./config";
 
-dotenv.config({path : path.join(process.cwd(), ".env")})
 
 const app = express();
-const port = 5000;
+const port = config.port;
 
 //parser
 app.use(express.json());
@@ -15,7 +13,7 @@ app.use(express.json());
 //DATABASE
 
 const pool = new Pool({
-  connectionString: `${process.env.CONNECTION_STRING}`,
+  connectionString: `${config.connection_str}`,
 });
 
 
