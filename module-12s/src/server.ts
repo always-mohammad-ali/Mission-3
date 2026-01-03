7ce68ec1413d6d69d338ab39bceb5704b4b67816
always-mohammad-ali <always.mohammad.ali@gmail.com>
2026-01-03 17:51:14 +0600

    created db.ts for cleaning and reusable purposes and it removed from server.ts then export the whole things

--- module-12s/src/server.ts
+++ module-12s/src/server.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
-import { Pool } from "pg";
 import config from "./config";
+import initDB, { pool } from "./config/db";
 
 
 const app = express();
@@ -12,46 +12,7 @@ app.use(express.json());
 
 //DATABASE
 
-const pool = new Pool({
-  connectionString: `${config.connection_str}`,
-});
 
-
-const initDB = async() =>{
-  try{
-       await pool.query(`
-          CREATE TABLE IF NOT EXISTS users(
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(100) NOT NULL,
-          email VARCHAR(150) UNIQUE NOT NULL,
-          age INT,
-          phone VARCHAR(15),
-          address TEXT,
-          created_at TIMESTAMP DEFAULT NOW(),
-          updated_at TIMESTAMP DEFAULT NOW()
-          )
-        `);
-        console.log("users table created");
-
-    await pool.query(`
-           CREATE TABLE IF NOT EXISTS todos(
-           id SERIAL PRIMARY KEY,
-           user_id INT REFERENCES users(id) ON DELETE CASCADE,
-           title VARCHAR(200) NOT NULL,
-           description TEXT,
-           completed BOOLEAN DEFAULT false,
-           due_date DATE,
-           created_at TIMESTAMP DEFAULT NOW(),
-           updated_at TIMESTAMP DEFAULT NOW()
-           )
-        `)
-        console.log("todos table created");
-  }
-  catch(error){
-    console.log("error message : ");
-  }
-
-}
 
 initDB();
 
